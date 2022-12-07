dd35e03c7ae9a4c993b75d2e6b66ad30b0e228ae
Yashid Mohamed <70381475+yashid-mohamed@users.noreply.github.com>
2022-12-07 01:09:25 +0000

    remove path

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-chrome_service = Service(ChromeDriverManager(path="/snap/bin/chromium",chrome_type=ChromeType.CHROMIUM).install())
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 chrome_options = Options()
 options = [
